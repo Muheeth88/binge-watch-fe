63221ab1165ae0a9b02eaa03f8f1a1f7e0d0a270
Abdul Muheeth <abdul.muheeth@megaops.io>
2024-01-18 01:32:29 +0530

    comments done

--- src/pages/MovieDetails/MovieDetails.jsx
+++ src/pages/MovieDetails/MovieDetails.jsx
@@ -5,18 +5,27 @@ import { format } from "date-fns";
 import { pink } from "@mui/material/colors";
 import EditIcon from "@mui/icons-material/Edit";
 import DeleteIcon from "@mui/icons-material/Delete";
+import TextField from "@mui/material/TextField";
+import Dialog from "@mui/material/Dialog";
+import DialogActions from "@mui/material/DialogActions";
+import DialogContent from "@mui/material/DialogContent";
+import DialogContentText from "@mui/material/DialogContentText";
+import DialogTitle from "@mui/material/DialogTitle";
+import Button from "@mui/material/Button";
 
 const MovieDetails = () => {
 	const { movieId } = useParams();
 	const [movie, setMovie] = useState({});
 	const [reviews, setReviews] = useState([]);
 	const username = localStorage.getItem("username");
-    const [comment, setComment] = useState("")
+	const [comment, setComment] = useState("");
+	const [open, setOpen] = useState(false);
+	const [commentId, setCommentId] = useState("");
 
 	useEffect(() => {
 		fetchMovieDetails();
 		fetchReviews();
-	}, [reviews]);
+	}, []);
 
 	const fetchMovieDetails = async () => {
 		try {
@@ -38,94 +47,157 @@ const MovieDetails = () => {
 		}
 	};
 
-    const editReview = async (reviewId) => {
-        try {
+	const handleCommentField = (e) => {
+		const comment = e.target.value;
+		setComment(comment);
+	};
 
-            const response = await api.patch(`reviews/edit-review/${reviewId}`)
-            console.log(response)
-        } catch (error) {
-            console.error(error.message)
-        }
-    }
+	const addReview = async () => {
+		try {
+			await api.post(`reviews/add-review/${movieId}`, { comment });
+			handleClose();
+		} catch (error) {
+			console.error(error.message);
+		}
+	};
 
-    const deleteReview = async(reviewId) => {
-        try {
-            const response = await api.delete(`reviews/delete-review/${reviewId}`)
-            console.log(response)
-        } catch (error) {
-            console.error(error.message)
-        }
-    }
+	const editReview = async () => {
+		try {
+			await api.patch(`reviews/edit-review/${commentId}`, { comment });
+			handleClose();
+		} catch (error) {
+			console.error(error.message);
+		}
+	};
+
+	const deleteReview = async (reviewId) => {
+		try {
+			const response = await api.delete(`reviews/delete-review/${reviewId}`);
+			console.log(response);
+		} catch (error) {
+			console.error(error.message);
+		}
+	};
+
+	const handleClickOpen = async (reviewId) => {
+		if (reviewId) {
+			setCommentId(reviewId);
+			const res = await api.get(`/reviews/get-review/${reviewId}`);
+			setComment(res.data.data.comment);
+		}
+		setOpen(true);
+	};
+
+	const handleClose = () => {
+		setOpen(false);
+	};
 
 	return (
-		<div className="flex justify-center">
-			<div className="flex flex-col justify-center">
-				<div className="flex flex-col md:flex-row max-w-7xl justify-center items-center ">
-					<div className="overflow-hidden w-full m-4 shadow-sm flex flex-col md:flex-row justify-center">
-						<div className="flex flex-col md:flex-row items-center">
-							<div className=" w-96 overflow-hidden">
-								<img src="https://source.unsplash.com/500x350/?city" alt="" className="" />
-							</div>
-							<div className="md:w-2/3 m-4 ">
-								<div className="flex text-gray-500 text-sm m-2">
-									<div className="m-1 font-bold">Released on</div>
-									{/* <div className="m-1">{format((new Date(movie?.releaseDate)), "dd MMMM yyyy")}</div> */}
+		<>
+			<div className="flex justify-center">
+				<div className="flex flex-col justify-center">
+					<div className="flex flex-col md:flex-row max-w-7xl justify-center items-center ">
+						<div className="overflow-hidden w-full m-4 shadow-sm flex flex-col md:flex-row justify-center">
+							<div className="flex flex-col md:flex-row items-center">
+								<div className=" w-96 overflow-hidden">
+									<img src="https://source.unsplash.com/500x350/?city" alt="" className="" />
 								</div>
-								<div className="font-bold text-black text-xl m-2">{movie.title}</div>
-								<div className="text-sm text-gray-500 mt-4 m-2">
-									<p>{movie.tagline}</p>
-								</div>
-								<div className="">
-									<p className="py-2">
-										<span className="mx-2 text-lg font-bold text-left">Language</span>
-										<span>{movie.originalLanguage}</span>
-									</p>
-									<p className="flex">
-										<span className="mx-2 text-lg font-bold text-left">Genre</span>
-										<span className="flex justify-center">
-											<span className="mx-1">{movie.genre}</span>
-										</span>
-									</p>
-									<p>
-										<span className="mx-2 text-lg font-bold text-left">Rating</span>
-										<span> 9/ 10</span>
-									</p>
+								<div className="md:w-2/3 m-4 ">
+									<div className="flex text-gray-500 text-sm m-2">
+										<div className="m-1 font-bold">Released on</div>
+										{/* <div className="m-1">{format((new Date(movie?.releaseDate)), "dd MMMM yyyy")}</div> */}
+									</div>
+									<div className="font-bold text-black text-xl m-2">{movie.title}</div>
+									<div className="text-sm text-gray-500 mt-4 m-2">
+										<p>{movie.tagline}</p>
+									</div>
+									<div className="">
+										<p className="py-2">
+											<span className="mx-2 text-lg font-bold text-left">Language</span>
+											<span>{movie.originalLanguage}</span>
+										</p>
+										<p className="flex">
+											<span className="mx-2 text-lg font-bold text-left">Genre</span>
+											<span className="flex justify-center">
+												<span className="mx-1">{movie.genre}</span>
+											</span>
+										</p>
+										<p>
+											<span className="mx-2 text-lg font-bold text-left">Rating</span>
+											<span> 9/ 10</span>
+										</p>
+									</div>
 								</div>
 							</div>
 						</div>
 					</div>
-				</div>
-				<div className="w-1/2">
-					<p className="text-xl font-bold">Comments</p>
-					<p className="flex justify-end">
-						<button className="button-blue text-end">+ Add Review</button>
-					</p>
-					{reviews &&
-						reviews.map((r) => (
-							<div key={r._id}>
-								<div className="flex justify-between">
-									<p>{r.comment}</p>
-									{username === r.reviewBy[0].userName && (
-										<span className="flex gap-2">
-											<span  onClick={() => editReview(r._id)}  className="icon">
-												<EditIcon color="primary" />
-											</span>
+					<div className="w-1/2">
+						<p className="text-xl font-bold">Comments</p>
+						<p className="flex justify-end">
+							<button onClick={() => handleClickOpen()} className="button-blue text-end mb-4">
+								+ Add Review
+							</button>
+						</p>
+						{reviews &&
+							reviews.map((r) => (
+								<div key={r._id}>
+									<div className="flex justify-between">
+										<p>{r.comment}</p>
+										{username === r.reviewBy[0].userName && (
+											<span className="flex gap-2">
+												<span onClick={() => handleClickOpen(r._id)} className="icon">
+													<EditIcon color="primary" />
+												</span>
 
-											<span onClick={() => deleteReview(r._id)} className="icon">
-												<DeleteIcon sx={{ color: pink[500] }} />
+												<span onClick={() => deleteReview(r._id)} className="icon">
+													<DeleteIcon sx={{ color: pink[500] }} />
+												</span>
 											</span>
-										</span>
-									)}
+										)}
+									</div>
+									<span className="ml-auto flex justify-start text-xs text-gray-400">
+										<span>-by {r.reviewBy[0].userName}</span>
+									</span>
+									<hr className="my-2" />
 								</div>
-								<span className="ml-auto flex justify-end text-sm text-gray-500">
-									<span>-by {r.reviewBy[0].userName}</span>
-								</span>
-								<hr className="my-2" />
-							</div>
-						))}
+							))}
+					</div>
 				</div>
 			</div>
-		</div>
+			<Dialog open={open} onClose={handleClose}>
+				<DialogTitle>{movie.title}</DialogTitle>
+				<DialogContent>
+					<DialogContentText>Add your comment here.</DialogContentText>
+					<TextField
+						sx={{ width: 500 }}
+						autoFocus
+						required
+						margin="dense"
+						id="name"
+						name="comment"
+						value={comment}
+						onChange={handleCommentField}
+						label="Comment"
+						type="text"
+						fullWidth
+						variant="standard"
+					/>
+				</DialogContent>
+				<DialogActions>
+					<Button onClick={handleClose}>Cancel</Button>
+					{commentId && (
+						<Button onClick={editReview} type="submit">
+							Edit Comment
+						</Button>
+					)}
+					{!commentId && (
+						<Button onClick={addReview} type="submit">
+							Post Comment
+						</Button>
+					)}
+				</DialogActions>
+			</Dialog>
+		</>
 	);
 };
 
